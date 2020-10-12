09a95069adccfa83bd4050e43c91c49f531e3f3d
mariusz79 <mpawlowic@gmail.com>
2020-10-12 20:32:48 +0100

    Add logo and menu

--- script/main.js
+++ script/main.js
@@ -1,4 +1,4 @@
-//option for magic mouse cursor
+//options for magic mouse cursor
 options = {
 	cursorOuter: "circle-basic",
 	hoverEffect: "circle-move",
@@ -17,4 +17,58 @@ function splashScreen() {
 		splashScreen.style.display = "none";
 	}, 2000);
 };
+
+
+//change cursor's appearance when on the right side
+const right = document.querySelector(".rightSide");
+const cursor = document.querySelector("#magicMouseCursor");
+const pointer = document.querySelector("#magicPointer");
+
+var para = document.createElement("span");
+var para1 = document.createElement("span");
+var para2 = document.createElement("span");
+var node = document.createTextNode("^");
+var node1 = document.createTextNode("View");
+var node2 = document.createTextNode("^");
+
+para.appendChild(node);
+para1.appendChild(node1);
+para2.appendChild(node2);
+
+right.addEventListener("mouseenter", () => {
+	pointer.style.height = "90px";
+	pointer.style.width = "90px";
+	cursor.classList.add("hide");
+	pointer.append(para, para1, para2);
+	pointer.classList.add("flex");
+});
+
+right.addEventListener("mouseleave", () => {
+	pointer.style.height = "5px";
+	pointer.style.width = "5px";
+	cursor.classList.remove("hide");
+	pointer.removeChild(para);
+	pointer.removeChild(para1);
+	pointer.removeChild(para2);
+});
+
+
+//menu
+const menu = document.querySelector(".menu");
+const menuOpen = document.querySelector(".menuOpen");
+
+var menuOpenClose = false;
+
+function openMenu() {
+	if (menuOpenClose == false) {
+		menuOpen.classList.remove("fade-out");
+		menuOpen.classList.add("scale");
+		menuOpenClose = true;
+	} else {
+		menuOpen.classList.remove("scale");
+		menuOpen.classList.add("fade-out");
+		menuOpenClose = false;
+	}
+}
+menu.addEventListener("click", openMenu);
 
