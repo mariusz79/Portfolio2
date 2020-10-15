031cfcb0efb92d433c81bb9bed67948be0b56494
mariusz79 <mpawlowic@gmail.com>
2020-10-15 19:12:41 +0100

    Refactoring js code

--- script/main.js
+++ script/main.js
@@ -1,3 +1,39 @@
+//after loading center view on second slide
+$(document).ready(function () {
+	$('.rightSide').scrollTop($(window).height() / 3);
+	
+});
+
+
+//wobble effect on windows
+var settings = {
+	name: "my_window", //name
+	depth: 0, //depth for zIndex
+	offsetX: 0, //+ or - value the size of the div
+	offsetY: 0, //+ or - value the size of the div
+	moveTypeIn: "move", //method points follow the mouse
+	moveTypeOut: "wobble", //method points go back to init position
+	wobbleFactor: 0.01, //control the wobble effect
+	wobbleSpeed: 0.1, //control the wobble speed
+	moveSpeed: 1, //control the move speed
+	lineWidth: 20, //lineWidth
+	lineColor: "#fff", //no value = no line. Use hex/rgba values
+	bodyColor: "", //no value = no body color. Use hex/rgba values
+	numberOfXPoints: 21, //quantity of points horizontal. must be an odd int
+	numberOfYPoints: 15, //quantity of points vertical. must be an odd int
+	movementLeft: true, //enable/disable movement directions
+	movementRight: false, //enable/disable movement directions
+	movementTop: true, //enable/disable movement directions
+	movementBottom: true, //enable/disable movement directions
+	autoResize: false, //if true size will be automatically adjusted
+	autoResize: false, //enable/disable automatic size adjustement
+	debug: false, //enable/disable debug mode
+};
+
+$("#window").wobbleWindow(settings);
+$("#window1").wobbleWindow(settings);
+$("#window2").wobbleWindow(settings);
+
 //options for magic mouse cursor
 options = {
 	cursorOuter: "circle-basic",
@@ -12,18 +48,42 @@ magicMouse(options);
 
 //welcome screen, reveal after website is loaded
 function splashScreen() {
-	var splashScreen = document.getElementById("splashscreen");
 	setTimeout(function () {
-		splashScreen.style.display = "none";
-	}, 2000);
+		$("#splashscreen").css('display', 'none');
+		$(".leftMiddleText").addClass("animateLeftMiddle");
+		$(".leftTop").addClass("fadein1animation");
+		$(".leftBottom").addClass("fadein1animation");
+	}, 3000);
+	setTimeout(function () {
+		$(".leftTop").addClass("opacity1");
+		$(".leftBottom").addClass("opacity1");
+	}, 5500);
 };
+
+// Wrap every letter in a span
+var textWrapper = document.querySelector('.ml12');
+textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+
+anime.timeline({loop: true})
+  .add({
+    targets: '.ml12 .letter',
+    translateX: [40,0],
+    translateZ: 0,
+    opacity: [0,1],
+    easing: "easeOutExpo",
+    duration: 1200,
+    delay: (el, i) => 500 + 30 * i
+  }).add({
+    targets: '.ml12 .letter',
+    translateX: [0,-30],
+    opacity: [1,0],
+    easing: "easeInExpo",
+    duration: 1100,
+    delay: (el, i) => 100 + 30 * i
+  });
 
 
 //change cursor's appearance when on the right side
-const right = document.querySelector(".rightSide");
-const cursor = document.querySelector("#magicMouseCursor");
-const pointer = document.querySelector("#magicPointer");
-
 var para = document.createElement("span");
 var para1 = document.createElement("span");
 var para2 = document.createElement("span");
@@ -35,40 +95,28 @@ para.appendChild(node);
 para1.appendChild(node1);
 para2.appendChild(node2);
 
-right.addEventListener("mouseenter", () => {
-	pointer.style.height = "90px";
-	pointer.style.width = "90px";
-	cursor.classList.add("hide");
-	pointer.append(para, para1, para2);
-	pointer.classList.add("flex");
+$(".rightSide").mouseenter(function () { 
+	$("#magicPointer").css({ 'width': '90px', 'height': '90px' }).addClass("flex");
+	$("#magicMouseCursor").addClass("hide");
+	$("#magicPointer").append(para, para1, para2);
 });
 
-right.addEventListener("mouseleave", () => {
-	pointer.style.height = "5px";
-	pointer.style.width = "5px";
-	cursor.classList.remove("hide");
-	pointer.removeChild(para);
-	pointer.removeChild(para1);
-	pointer.removeChild(para2);
+$(".rightSide").mouseleave(function () {
+	$("#magicPointer").css({ width: "5px", height: "5px" });
+	$("#magicMouseCursor").removeClass("hide");
+	$("#magicPointer").empty();
+
 });
 
 
 //menu
-const menu = document.querySelector(".menu");
-const menuOpen = document.querySelector(".menuOpen");
-
-var menuOpenClose = false;
-
-function openMenu() {
-	if (menuOpenClose == false) {
-		menuOpen.classList.remove("fade-out");
-		menuOpen.classList.add("scale");
-		menuOpenClose = true;
-	} else {
-		menuOpen.classList.remove("scale");
-		menuOpen.classList.add("fade-out");
-		menuOpenClose = false;
-	}
-}
-menu.addEventListener("click", openMenu);
-
+var menuOpen = false;
+$('.showMenu').click(function () {
+	if (menuOpen == false) {
+			$(".menu").addClass("scale").removeClass("fade-out");
+			menuOpen = !menuOpen;
+	} else if (menuOpen == true) {
+			$(".menu").addClass("fade-out").removeClass("scale");
+			menuOpen = !menuOpen;
+		}
+});
